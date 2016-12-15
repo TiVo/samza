32dd7f16f38a5e8df2a51c240ae5bae9879dc67b
Tommy Becker <tobecker@tivo.com>
2016-12-15 08:52:52 -0500

    Revert "SAMZA-770: HttpServer uses getHostAddress instead of getHostName to build URL"
    This reverts commit 3fb8d6f41199815083c25452d3442fca7110f8d8.

--- samza-core/src/main/scala/org/apache/samza/coordinator/server/HttpServer.scala
+++ samza-core/src/main/scala/org/apache/samza/coordinator/server/HttpServer.scala
@@ -130,8 +130,7 @@ class HttpServer(
   def getUrl = {
     if (running) {
       val runningPort = server.getConnectors()(0).asInstanceOf[NetworkConnector].getLocalPort()
-
-      new URL("http://" + Util.getLocalHost.getHostName + ":" + runningPort + rootPath)
+      new URL("http://" + Util.getLocalHost.getHostAddress + ":" + runningPort + rootPath)
     } else {
       throw new SamzaException("HttpServer is not currently running, so URLs are not available for it.")
     }
